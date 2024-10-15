1b9728279dbf58510c66a98638043e501953885c
Sanath2349 <sanathreddyg42@gmail.com>
2024-10-15 10:11:41 +0530

    redux

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -2,7 +2,9 @@ import { configureStore } from '@reduxjs/toolkit';
 import rootReducer from './rootReducer';
 
 const store = configureStore({
-  reducer: rootReducer,
+  reducer: {
+    user: rootReducer,
+  },
 });
 
 export default store;
